79fa6f61852c35412ea2b4ac2607ee08fbb3c4f3
repl.it user <replituser@example.com>
2020-11-26 20:53:57 +0000

    update, testcases still need fix

--- Life.java
+++ Life.java
@@ -1,145 +1,129 @@
 public class Life implements ILife {
 
-    boolean[][] generations = null;
+  boolean[][] generations = new boolean[15][15];
 
-    public static void main(String[] args) {
-        Life l = new Life(new String[] {
-            "     ",
-            "     ",
-            " *** ",
-            "     ",
-            "     "
-        });
-        l = (Life) l.nextGeneration();
+  public static void main(String[] args) {
+
+    Life l = new Life(new String[] {
+      "***  ",
+      "     ",
+      "     ",
+      "     ",
+      "     "
+    });
+    l = (Life) l.nextGeneration();
+  }
+
+  public Life() {
+    nukeAll();
+  }
+
+  public Life(String[] setup) {
+    this();
+    for (int y = 0; y < setup.length; y++)
+    for (int x = 0; x < setup[y].length(); x++)
+    if (setup[y].charAt(x) != ' ') setAlive(x, y);
+  }
+
+  public int getDimX(int index) {
+    return generations[index].length;
+
+  }
+
+  public int getDimY() {
+    return generations.length;
+
+  }@Override
+  public void nukeAll() {
+
+    for (int i = 0; i < getDimY(); i++) {
+      for (int j = 0; j < getDimX(i); j++) {
+        generations[i][j] = false;
+      }
     }
+  }
 
+  @Override
+  public void setAlive(int x, int y) {
+    // TODO Auto-generated method stub
+    generations[x][y] = true;
+  }
 
-    public Life() {
+  @Override
+  public void setDead(int x, int y) {
+    // TODO Auto-generated method stub
+    generations[x][y] = false;
+  }
 
-        generations = new boolean[10][10];
-        nukeAll();
-    }
+  @Override
+  public boolean isAlive(int x, int y) {
 
-    public Life(String[] setup) {
-        this();
-
-        generations = new boolean[setup.length][setup[0].length()];
-        for (int y = 0; y < setup.length; y++)
-            for (int x = 0; x < setup[y].length(); x++)
-                if (setup[y].charAt(x) != '.')
-                    setAlive(x, y);
-                else
-                    setDead(x, y);
-
-    }
-
-    public Life(boolean[][] nextGeneration) {
-
-        this();
-
-        generations = nextGeneration;
-    }
-
-    @Override
-    public void nukeAll() {
-        if (generations == null) {
-            throw new IllegalArgumentException("Generations is NULL");
-
-        }
-
-        for (int i = 0; i < generations.length; i++) {
-            int line = generations[i].length;
-            for (int j = 0; j < line; j++) {
-
-                generations[j][i] = false;
-            }
-        }
-    }
-
-    @Override
-    public void setAlive(int x, int y) {
-        // TODO Auto-generated method stub
-        generations[x][y] = true;
-    }
-
-    @Override
-    public void setDead(int x, int y) {
-        // TODO Auto-generated method stub
-        generations[x][y] = false;
-    }
-
-    @Override
-    public boolean isAlive(int x, int y) {
-
-
+    if (x >= 0 && x < getDimY()) {
+      if (y >= 0 && y < getDimX(x)) {
         return generations[x][y];
+      }
     }
+    return false;
+  }
 
-    @Override
-    public ILife nextGeneration() {
-        boolean[][] nextGeneration = new boolean[generations.length][generations[0].length];
+  @Override
+  public ILife nextGeneration() {
+    boolean[][] nextGeneration = new boolean[getDimY()][getDimX(0)];
 
-        for (int y = 0; y < generations.length; y++) {
+    for (int y = 0; y < getDimY(); y++) {
 
-            int line = generations[y].length;
+      int line = getDimX(y);
 
-            for (int x = 0; x < line; x++) {
+      for (int x = 0; x < line; x++) {
 
+        int tmp = countNeighbours(x, y);
+        if (isAlive(x, y)) {
 
-                int tmp = countNeighbours(x, y);
-                if (isAlive(x, y)) {
-
-                    if (tmp == 2 || tmp == 3) {
-                        nextGeneration[x][y] = true;
-                    } else {
-                        nextGeneration[x][y] = false;
-                    }
-                } else {
-                    if (tmp == 3) {
-                        nextGeneration[x][y] = true;
-                    } else {
-                        nextGeneration[x][y] = false;
-                    }
-                }
-            }
+          if (tmp == 2 || tmp == 3) {
+            nextGeneration[x][y] = true;
+          } else {
+            nextGeneration[x][y] = false;
+          }
+        } else {
+          if (tmp == 3) {
+            nextGeneration[x][y] = true;
+          } else {
+            nextGeneration[x][y] = false;
+          }
         }
-        return new Life(nextGeneration);
+      }
     }
+    return this;
+  }
 
+  public int countNeighbours(int x, int y) {
+    int neighbours = 0;
 
-
-
-    public int countNeighbours(int x, int y) {
-        int neighbours = 0;
-
-        if (isAlive(x, y + 1)) {
-            neighbours++;
-        }
-        if (isAlive(x - 1, y + 1)) {
-            neighbours++;
-        }
-        if (isAlive(x - 1, y)) {
-            neighbours++;
-        }
-        if (isAlive(x - 1, y - 1)) {
-            neighbours++;
-        }
-        if (isAlive(x, y - 1)) {
-            neighbours++;
-        }
-        if (isAlive(x + 1, y - 1)) {
-            neighbours++;
-        }
-        if (isAlive(x + 1, y)) {
-            neighbours++;
-        }
-        if (isAlive(x + 1, y + 1)) {
-            neighbours++;
-        }
-        return neighbours;
+    if (isAlive(x, y + 1)) {
+      neighbours++;
     }
-
-
-
+    if (isAlive(x - 1, y + 1)) {
+      neighbours++;
+    }
+    if (isAlive(x - 1, y)) {
+      neighbours++;
+    }
+    if (isAlive(x - 1, y - 1)) {
+      neighbours++;
+    }
+    if (isAlive(x, y - 1)) {
+      neighbours++;
+    }
+    if (isAlive(x + 1, y - 1)) {
+      neighbours++;
+    }
+    if (isAlive(x + 1, y)) {
+      neighbours++;
+    }
+    if (isAlive(x + 1, y + 1)) {
+      neighbours++;
+    }
+    return neighbours;
+  }
 
 }
--- LifeTest.java
+++ LifeTest.java
@@ -1,38 +1,80 @@
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert. * ;
 
 public class LifeTest {
-    
-    @Test
-    public void createNewCell() {
-        
-        // Arrange: drei lebende Zellen
-        Life l = new Life();
-        l.setAlive(0, 0);
-        l.setAlive(0, 1);
-        l.setAlive(0, 2);
 
-        // Act: Berechnung der Folgegeneration
-        ILife nextGen = l.nextGeneration();
+  @Test
+  public void createNewCell() {
 
-        // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
-        assertTrue(nextGen.isAlive(1, 1));
-    }
+    // Arrange: drei lebende Zellen
+    Life l = new Life();
+    l.setAlive(0, 0);
+    l.setAlive(0, 1);
+    l.setAlive(0, 2);
 
+    // Act: Berechnung der Folgegeneration
+    ILife nextGen = l.nextGeneration();
 
-    @Test
-    public void destroyLonelyCell() {
-    }
+    // Assert: Rasterpunkt mit drei Nachbarn sollte jetzt leben
+    assertTrue(nextGen.isAlive(1, 1));
+  }
 
+  @Test
+  public void destroyLonelyCell() {
 
-    @Test
-    public void keepAliveCell() {
-    }
+    Life living = new Life();
+    living.setAlive(5, 0);
+    living.setAlive(5, 1);
+    ILife nextGen = living.nextGeneration();
+    assertTrue(!nextGen.isAlive(5, 1));
+  }
 
+  @Test
+  public void keepAliveCell() {
 
-    @Test
-    public void destroyCrowdedCell() {
-    }
+    Life living = new Life();
+    living.setAlive(5, 0);
+    living.setAlive(5, 1);
+    living.setAlive(5, 2);
+    ILife nextGen = living.nextGeneration();
+    assertTrue(nextGen.isAlive(5, 1));
+  }
 
+  @Test
+  public void keepAliveCell2() {
+    Life l = new Life();
+    l.setAlive(0, 0);
+    l.setAlive(0, 1);
+    l.setAlive(0, 2);
+    ILife nextGen = l.nextGeneration();
+    assertTrue(nextGen.isAlive(0, 1));
+  }
+
+  @Test
+  public void keepAliveCell3() {
+    Life l = new Life();
+    l.setAlive(0, 0);
+    l.setAlive(0, 1);
+    l.setAlive(0, 2);
+    l.setAlive(1, 1);
+    ILife nextGen = l.nextGeneration();
+    assertTrue(nextGen.isAlive(0, 0));
+    assertTrue(nextGen.isAlive(0, 1));
+    assertTrue(nextGen.isAlive(0, 2));
+    assertTrue(nextGen.isAlive(1, 1));
+  }
+
+  @Test
+  public void destroyCrowdedCell() {
+
+    Life living = new Life();
+    living.setAlive(5, 0);
+    living.setAlive(5, 1);
+    living.setAlive(5, 2);
+    living.setAlive(6, 0);
+    living.setAlive(6, 1);
+    ILife nextGen = living.nextGeneration();
+    assertTrue(!nextGen.isAlive(5, 1));
+  }
 
 }
